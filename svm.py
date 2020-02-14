aef8cb8fef6dacea7ccc0e0ea47a7bd2ee94a00a
Kirk Easterson <kirk.easterson@gmal.com>
2020-02-14 12:56:50 +0100

    added all of things to implement, debugging still needs to be done, nonzero arrays need to be completed

--- svm.py
+++ svm.py
@@ -8,6 +8,12 @@ import numpy.linalg as la
 import scipy as sp
 from scipy.optimize import minimize
 
+# type of kernel to be used
+#   1 - linear
+#   2 - polynomial
+#   3 - rbf
+kernel_type = 1
+
 
 # global variables
 C = 1
@@ -17,16 +23,53 @@ t = np.zeros(N)
 p = np.zeros((N, N))
 alphas = np.zeros(N)
 
+nonzero_x = []
+nonzero_t = []
+nonzero_p = []
 
-# equation 4
+# bias, along with s and t_s used to calculate it
+b = 0
+s = 0
+t_s = 0
+
+
+# equation 4: objective function
 def objective(alphas):
     global p
-    return (1/2)*(np.dot(alphas, np.dot(alphas, p))) - np.sum(alphas)
+    return (1/2)*(np.sum(alphas, np.dot(alphas, p))) - np.sum(alphas)
+
+
+# equation 6: indicator function
+def ind(s):
+    return np.dot(alphas, np.dot(t, [kernel(s, x[i]) for i in range(x.size)])) - b
+
+
+# equation 7: threshold function to calculate bias
+def bias():
+    global s
+    global t_s
+    while (s == 0):
+        index = random.randint(0, alphas.size)
+        s = alphas[index]
+        t_s = t[index]
+    b = np.dot(alphas, np.dot(t, [kernel(s, x[i])
+                                 for i in range(x.size)])) - t_s
 
 
 # equality constraint of (10)
 def zerofun(alphas):
     return np.dot(alphas, t)
+
+
+# switcher function for different kernel types
+def kernel(p1, p2):
+    switcher = {
+        1: linear_kernel,
+        2: polynomial_kernel,
+        3: rbf_kernel
+    }
+    func = switcher.get(kernel_type, linear_kernel)
+    return func(p1, p2)
 
 
 # linear kernel
@@ -46,28 +89,21 @@ def rbf_kernel(p1, p2):
     return math.e ** (-1 * (la.norm(p1-p2)**2) / (2*sigma**2))
 
 
+# precompute p
 def calculatematrixp():
     global p
     for i in range(0, t.size):
         for j in range(0, t.size):
-            p[i][j] = t[i] * t[j] * linear_kernel(x[i], x[j])
-
-
-# equation 6
-def indicator(s):
-    result = 0
-    for i in range(N):
-        result += alphas[i]*t[i]*linear_kernel(s, x[i])
-    return (result - b)
+            p[i][j] = t[i] * t[j] * kernel(x[i], x[j])
 
 
 def main():
 
-    # alphas = np.zeros(N)
     constraint = {'type': 'eq', 'fun': zerofun}
     bounds = [(0, C) for b in range(N)]
 
     calculatematrixp()
+    bias()
 
     ret = minimize(objective, alphas, bounds, constraint)
     alpha = ret['x']
