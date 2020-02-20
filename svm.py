21e12e85f9ee32e56763766295214aa3909e0e45
Kirk Easterson <kirk.easterson@gmal.com>
2020-02-20 17:49:02 +0100

    temp

--- svm.py
+++ svm.py
@@ -10,28 +10,28 @@ import scipy as sp
 from numpy.random import randn
 from scipy.optimize import minimize
 
-
 # type of kernel to be used
 #   1 - linear
 #   2 - polynomial
 #   3 - rbf
-kernel_type = 3
+kernel_type = 1
 
-
-# global variables
-classA = []
-classB = []
-inputs = []
-targets = []
-permute = []
-
+# for data generation
+classA = np.empty((0, 0))
+classB = np.empty((0, 0))
+inputs = np.empty((0, 0))
+targets = np.empty((0, 0))
+permute = ()
 
 C = 1
-N = 0
-p = []
-alphas = []
+N = 0  # number of points
+b = 0  # bias
+data_points = np.empty(0)
+t = np.empty(0)
+p = np.empty((0, 0))
+alphas = np.empty(0)
 
-nonzero_x = []
+nonzero_data_points = []
 nonzero_t = []
 nonzero_p = []
 
@@ -44,33 +44,31 @@ t_s = 0
 # equation 4: objective function
 def objective(alphas):
     global p
-    return (1/2)*(np.sum(np.dot(np.dot(alphas, p), alphas))) - np.sum(alphas)
+    return (1 / 2) * (np.sum(alphas, np.dot(alphas, p))) - np.sum(alphas)
 
 
 # equation 6: indicator function
 def indicator(s):
-    return np.dot(np.multiply(alphas, targets), [kernel(s, inputs[i]) for i in range(len(inputs))]) - b
+    # def indicator():
+    return np.dot(alphas, np.dot(t, [kernel(s, data_points[i]) for i in range(data_points.size)])) - b
 
 
 # equation 7: threshold function to calculate bias
 def bias():
-    global b
     global s
     global t_s
-    global alphas
-    global inputs
     while s == 0:
-        index = random.randint(0, len(alphas) - 1)
+        index = random.randint(0, alphas.size)
         s = alphas[index]
-        t_s = targets[index]
-    s = inputs[index]          '''changed'''
-    b = np.dot(np.multiply(alphas, targets), [kernel(s, inputs[i]) for i in range(len(inputs))]) - t_s
-
+        t_s = t[index]
+    b = np.dot(alphas, np.dot(t, [kernel(s, data_points[i])
+                                  for i in range(data_points.size)])) - t_s
 
 
 # equality constraint of (10)
-def zerofun(alphas):
-    return np.dot(alphas, targets)
+# def zerofun(alphas):
+def zerofun():
+    return np.dot(alphas, t)
 
 
 # switcher function for different kernel types
@@ -86,40 +84,24 @@ def kernel(p1, p2):
 
 # linear kernel
 def linear_kernel(p1, p2):
-    return np.dot(p1, p2)
-
-'''transposing a 1D array returns the array unchanged'''
+    return np.dot(p1.transpose(), p2)
 
 
 # polynomial kernel
 def polynomial_kernel(p1, p2):
-    p = 2
-    return (np.dot(p1, p2) + 1) ** p
+    p = 2  # controls degree of polynomial, the higher the more complex the shape
+    return (np.dot(p1.transpose(), p2) + 1) ** p
 
 
 # radial basis function kernel
 def rbf_kernel(p1, p2):
     sigma = 1
-    return math.e ** (-1 * (la.norm(p1-p2)**2) / (2*sigma**2))
-
-
-# precompute p
-def calculatematrixp():
-    global p
-    p = np.zeros((len(targets), len(targets)))
-    for i in range(0, len(targets)):
-        for j in range(0, len(targets)):
-            p[i][j] = targets[i] * targets[j] * kernel(inputs[i], inputs[j])
+    return math.e ** (-1 * (la.norm(p1 - p2) ** 2) / (2 * (sigma ** 2)))
 
 
 # generate the test data
 def generate_data():
-    global classA
-    global classB
-    global inputs
-    global targets
-    global permute
-    global N
+    global classA, classB, inputs, targets, permute, N
 
     # numpy.random.seed(100)
     classA = np.concatenate(
@@ -140,26 +122,44 @@ def generate_data():
     targets = targets[permute]
 
 
+# initialize containers
+def init():
+    global data_points, t, p, alphas
+    data_points = np.zeros(N)
+    t = np.zeros(N)
+    p = np.zeros((N, N))
+    alphas = np.zeros(N)
+
+
+# precompute p
+def calculatematrixp():
+    global p
+    for i in range(0, t.size):
+        for j in range(0, t.size):
+            p[i][j] = t[i] * t[j] * kernel(data_points[i], data_points[j])
+
+
 # plot the data
 def plot_data():
-    plt.plot([p[0] for p in classA],
-             [p[1] for p in classA],
+    plt.plot([pt[0] for pt in classA],
+             [pt[1] for pt in classA],
              'b.')
 
-    plt.plot([p[0] for p in classB],
-             [p[1] for p in classB],
+    plt.plot([pt[0] for pt in classB],
+             [pt[1] for pt in classB],
              'r.')
 
     plt.axis('equal')  # force same scale on both axes
     plt.savefig('svmplot.pdf')  # save a copy in a file
-    '''plt.show()  # show the plot on the screen'''
+    plt.show()  # show the plot on the screen
+
 
 # plot the decsion boundary
 def plot_decision_boundary():
     xgrid = np.linspace(-5, 5)
     ygrid = np.linspace(-4, 4)
 
-    grid = np.array([[indicator([x, y])
+    grid = np.array([[indicator(x, y)
                       for x in xgrid]
                      for y in ygrid])
 
@@ -167,25 +167,22 @@ def plot_decision_boundary():
                 (-1.0, 0.0, 1.0),
                 colors=('red', 'black', 'blue'),
                 linewidths=(1, 3, 1))
-    plt.show()
+
 
 def main():
-
-    global alphas, inputs, C
-    cts = {'type': 'eq', 'fun': zerofun}
-
     generate_data()
-    alphas = np.zeros(len(inputs))
-    bds = [(0, C) for b in range(len(inputs))]
-
+    init()
     calculatematrixp()
+    bias()
 
-    ret = minimize(objective, alphas, bounds=bds, constraints=cts)
-    alphas = ret['x']
+    constraint = {'type': 'eq', 'fun': zerofun}
+    bounds = [(0, C) for b in range(N)]
+
+    ret = minimize(objective, alphas, bounds, constraint)
+    alpha = ret['x']
     if ret['success']:
         print("Success!")
 
-    bias()
     plot_data()
     plot_decision_boundary()
 
